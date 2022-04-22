f92a52027edef8bb129acd155bd45229874f433d
Parag <paragmb@hotmail.com>
2022-04-22 09:53:20 -0500

    <Steps 1 and 2 completed>

--- pychain.py
+++ pychain.py
@@ -50,7 +50,11 @@ import hashlib
 # Create a Record Data Class that consists of the `sender`, `receiver`, and
 # `amount` attributes
 # YOUR CODE HERE
-
+@dataclass
+class Record:
+    sender: str
+    receiver: str
+    amount: float
 
 ################################################################################
 # Step 2:
@@ -68,8 +72,8 @@ class Block:
 
     # @TODO
     # Rename the `data` attribute to `record`, and set the data type to `Record`
-    data: Any
-
+    #data: Any
+    record: Record
     creator_id: int
     prev_hash: str = "0"
     timestamp: str = datetime.datetime.utcnow().strftime("%H:%M:%S")
